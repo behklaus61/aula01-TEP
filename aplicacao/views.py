13e8cf9d243d1f9fb087d6801d81e6880fbbefd5
Brendha <brendhaklaus61@gmail.com>
2026-02-11 18:21:00 -0300

    pagina com botões funcionando

--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    context= {'curso': 'desenvolvimento de sistemas'}
+    return render(request, 'index.html', context)
+
+def contato(request):
+    context= {'nome': 'Instituto Federal de Santa Catarina',
+              'telefone': 'Telefone: (47) 3363-5251', 
+              'email': 'E-mail: contato@ifsc.edu.br'}
+    return render(request, 'contato.html', context)
